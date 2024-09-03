68e39cf0c7b3379119aef534027c273da0b6a845
GScience <GM2000@outlook.com>
2024-09-03 15:21:43 +0800

    修复cppcheck找不到时的错误 移除不需要的测试代码

--- Source/Engine/Core/src/Application.cpp
+++ Source/Engine/Core/src/Application.cpp
@@ -9,23 +9,6 @@
 #include <iostream>
 
 namespace Omnity::Core {
-	void SpeedTest() {
-		while (true) {
-			auto begin = std::chrono::high_resolution_clock::now();
-			for (int i = 0; i < 10000000; ++i) {
-				std::u16string str(u"1234");
-			}
-			auto end = std::chrono::high_resolution_clock::now();
-			auto elapsed1 = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
-			begin = std::chrono::high_resolution_clock::now();
-			for (int i = 0; i < 10000000; ++i) {
-				String str(u"1234");
-			}
-			end = std::chrono::high_resolution_clock::now();
-			auto elapsed2 = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
-			std::cout << "1: " << elapsed1 << ";" << elapsed2 << std::endl;
-		}
-	}
 	void Setup(const std::vector<String>& args) {
 		auto str = String(u"12🌹34");
 		auto str2 = std::move(str);
@@ -38,7 +21,6 @@ namespace Omnity::Core {
 			Logger::LogInfo(arg);
 		Omnity::Graphic::InitGraphic();
 		Omnity::Graphic::SetupGraphicBackend();
-		SpeedTest();
 	}
 	void Application::Run(int argc, const char* argv[]) {
 		std::vector<String> args;
