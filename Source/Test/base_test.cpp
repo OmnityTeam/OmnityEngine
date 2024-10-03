27a0432e8a7b396dd55dd1bcb5c5c989b489fed4
GScience <GM2000@outlook.com>
2024-10-03 22:37:34 +0800

    加测试预设

--- Source/Test/base_test.cpp
+++ Source/Test/base_test.cpp
@@ -11,7 +11,7 @@ TEST(base, type_system) {
 	ASSERT_EQ(omnity::type_id<omnity::material>, omnity::type_id_by_index<omnity::type_index<omnity::material>>);
 	ASSERT_EQ(omnity::type_index<omnity::shader>, omnity::type_index_by_id<omnity::type_id<omnity::shader>>);
 	ASSERT_EQ(omnity::type_id<omnity::shader>, omnity::type_id_by_index<omnity::type_index<omnity::shader>>);
-
+	
 	auto type_metadata = omnity::get_type_table()->try_get_type_metadata_by_id(omnity::type_id<omnity::texture>);
 	ASSERT_EQ(omnity::type_id<omnity::texture>, type_metadata->type_id);
 	ASSERT_EQ(omnity::get_type_table()->get_type_metadata<omnity::texture>()->type_id, type_metadata->type_id);
